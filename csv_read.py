63700e13f21a360898754e6bcf2ec4992262d7d6
y_yamaguchi <jamis.icon1137@gmail.com>
2021-06-02 17:49:52 +0900

    csv_read.py

--- csv_read.py
+++ csv_read.py
@@ -19,8 +19,32 @@ print(header)
 a = next(f)
 print(a)
 
-#辞書形式
-g = csv.DictReader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+
+time_list = []
+latitude = []
+latitude_direction = []
+longitude = []
+longitude_direction = []
+quality_posi = []
+satellite = []
+quality_horizontal = []
+antenna_height = []
+geoid = []
+thread_time = []
 
 
-
+for row in f:
+    
+    time_list.append([row[1]])
+    latitude.append([row[2]])
+    latitude_direction.append([row[3]])
+    longitude.append([row[4]])
+    longitude_direction.append([row[5]])
+    quality_posi.append([row[6]])
+    satellite.append([row[7]])
+    quality_horizontal.append([row[8]])
+    antenna_height.append([row[9]])
+    geoid.append([row[11]])
+    thread_time_cal  = float(row[18])+float(row[19])/60+float(row[20])/3600
+    thread_time.append([thread_time_cal])
+    
